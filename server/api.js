213117afee9ceae88082e7d720c93305247a0156
Sahad Saleel <saleelsahad@gmail.com>
2026-02-01 22:28:39 +0530

    Use Render backend URL

--- server/api.js
+++ server/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.3:5000/api',
+    baseURL: 'https://YOUR-RENDER-URL.onrender.com/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
